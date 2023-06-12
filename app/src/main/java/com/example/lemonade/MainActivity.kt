7e225b0c837b613e94da8aa631c623c10158d3a1
CheoR <cheo.roman+github@gmail.com>
2023-06-12 12:19:00 -0500

    build: step 3 and 4, squeeze lemons and drink lemonade.

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,8 +16,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,45 +60,96 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Screen(modifier: Modifier = Modifier) {
     var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
     when (currentStep) {
         1 -> {
-            Column (
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxSize()
-            ){
-                Text(text = stringResource(R.string.lemon_tree))
-                Spacer(modifier = Modifier.height(32.dp))
-                Image(
-                    painter = painterResource(R.drawable.lemon_tree),
-                    contentDescription = stringResource(R.string.lemon_tree),
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .clickable {
-                            currentStep = 2
-                        }
-                )
-            }
+            LemonTextAndImage(
+                textLabelResourceId = R.string.select_lemon,
+                drawableResourceId = R.drawable.lemon_tree,
+                contentDescriptionResourceId = R.string.select_lemon,
+                onImageClick = {
+                    currentStep = 2
+                    squeezeCount = (2..4).random()
+                }
+            )
         }
         2 -> {
-            Column (
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxSize()
-            ){
-                Text(text = stringResource(R.string.squeeze_lemon))
-                Spacer(modifier = Modifier.height(32
-                    .dp))
-                Image(
-                    painter = painterResource(R.drawable.lemon_squeeze),
-                    contentDescription = stringResource(R.string.squeeze_lemon),
-                    modifier = Modifier.wrapContentSize()
-                )
-            }
+            LemonTextAndImage(
+                textLabelResourceId = R.string.squeeze_lemon,
+                drawableResourceId = R.drawable.lemon_squeeze,
+                contentDescriptionResourceId = R.string.squeeze_lemon,
+                onImageClick = {
+                    squeezeCount--
+                    if (squeezeCount == 0) {
+                        currentStep = 3
+                    }
+                }
+            )
+        }
+
+        3 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.drink_lemonade,
+                drawableResourceId = R.drawable.lemon_drink,
+                contentDescriptionResourceId = R.string.drink_lemonade,
+                onImageClick = {
+                    currentStep = 4
+                }
+            )
+        }
+        4 -> {
+            LemonTextAndImage(
+                textLabelResourceId = R.string.empty_glass,
+                drawableResourceId = R.drawable.lemon_restart,
+                contentDescriptionResourceId = R.string.empty_glass,
+                onImageClick = {
+                    currentStep = 1
+                }
+            )
         }
     }
 
 }
+
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Button(
+                onClick = onImageClick,
+                shape = RoundedCornerShape(40.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+            ) {
+                Image(
+                    painter = painterResource(drawableResourceId),
+                    contentDescription = stringResource(contentDescriptionResourceId),
+                    modifier = Modifier
+                        .width(128.dp)
+                        .height(160.dp)
+                        .padding(24.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = stringResource(textLabelResourceId),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
 
 @Composable
 fun Heading(
@@ -144,66 +200,3 @@ fun GreetingPreview(modifier: Modifier = Modifier) {
         Screen()
     }
 }
-
-
-
-//Increase the font size of the text so that it's larger than the default font size (such as 18sp).
-//Add additional space in between the text label and the image below it, so they're not too close to each other (such as 16dp).
-//Give the button an accent color and slightly rounded corners to let the users know that they can tap the image.
-
-
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            DiceRollerTheme {
-//                DiceRollerApp()
-//            }
-//        }
-//    }
-//}
-//
-//@Preview
-//@Composable
-//fun DiceRollerApp() {
-//    DiceWithButtonAndImage()
-//}
-//
-//@Composable
-//fun DiceWithButtonAndImage(
-//    modifier: Modifier = Modifier
-//        .fillMaxSize()
-//        .wrapContentSize(Alignment.Center),
-//) {
-//    // mutableStateOf - rerender on state change
-//    // remember - composable
-//    var result by remember { mutableStateOf(1) }
-//
-//    val imageResource = when (result) {
-//        1 -> R.drawable.dice_1
-//        2 -> R.drawable.dice_2
-//        3 -> R.drawable.dice_3
-//        4 -> R.drawable.dice_4
-//        5 -> R.drawable.dice_5
-//        else -> R.drawable.dice_6
-//    }
-//
-//    Column(
-//        modifier=modifier,
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//    ) {
-//
-//        Image(
-//            painter = painterResource(id = imageResource),
-//            contentDescription = result.toString()
-//        )
-//
-//        Spacer(modifier = Modifier.height(16.dp))
-//
-//        Button(onClick = { result = (1..6).random()  }) {
-//            Text(stringResource(R.string.roll))
-//        }
-//
-//    }
-//
-//}
